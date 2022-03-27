c3a5e9288ce0d87b5ac67f965261041516becf8f
Vinamra Yadav <73683711+MythicalMAxX@users.noreply.github.com>
2022-03-27 21:24:11 +0530

    Update recurssion.c

--- unit 4/recurssion.c
+++ unit 4/recurssion.c
@@ -1,1 +1,21 @@
-writing
+#Program Name: Program to find product of n!.
+
+
+#include<stdio.h>
+
+int product(int num){
+if (num<=1)
+    return num;
+else
+    return num*product(num-1);
+
+}
+
+int main(){
+    int num1, num2;
+    printf("Enter the terms");
+    scanf("%d",&num1);
+    num2 = product(num1);
+    printf("%d",num2);
+    return 0;
+}
